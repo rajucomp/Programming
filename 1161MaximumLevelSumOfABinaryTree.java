f1ea1d97fbca17d4c2ec2ab2ebb161bba994d0fe
Raju Gupta <cs135017@mnnit.ac.in>
2022-06-06 01:43:16 +0200

    Create 1161MaximumLevelSumOfABinaryTree.java

---
+++ 1161MaximumLevelSumOfABinaryTree.java
@@ -0,0 +1,73 @@
+//https://leetcode.com/problems/maximum-level-sum-of-a-binary-tree/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int bfs(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList();
+        queue.add(root);
+        int minLevelSum = Integer.MIN_VALUE, minLevel = 1, level = 1;
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+            int currentLevelSum = 0;
+            for(int i = 0; i < size; i++) {
+                TreeNode node = queue.remove();
+                currentLevelSum += node.val;
+                if(node.left != null) {
+                    queue.add(node.left);
+                }
+                if(node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+            if(minLevelSum < currentLevelSum) {
+                minLevelSum = currentLevelSum;
+                minLevel = level;
+            }
+            level++;
+        }
+        return minLevel;
+    }
+    
+    public int maxLevelSum(TreeNode root) {
+        //return bfs(root);
+        return dfs(root);
+    }
+    
+    int dfs(TreeNode root) {
+        Map<Integer, Integer> levelSumMap = new HashMap();
+        dfs(root, levelSumMap, 1);
+        int maxLevelSum = root.val, minLevel = 1;
+        for(Map.Entry<Integer, Integer> entry : levelSumMap.entrySet()) {
+            if(entry.getValue() > maxLevelSum) {
+                maxLevelSum = entry.getValue();
+                minLevel = entry.getKey();
+            } else if(entry.getValue() == maxLevelSum) {
+                minLevel = Math.min(minLevel, entry.getKey());
+            }
+        }
+        return minLevel;
+    }
+    
+    void dfs(TreeNode root, Map<Integer, Integer> levelSumMap, int currentLevel) {
+        if(root == null) {
+            return;
+        }
+        levelSumMap.put(currentLevel, levelSumMap.getOrDefault(currentLevel, 0) + root.val);
+        dfs(root.left, levelSumMap, currentLevel + 1);
+        dfs(root.right, levelSumMap, currentLevel + 1);
+    }
+}
